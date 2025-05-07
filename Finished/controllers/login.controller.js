d6cc89274c82750fffb9c8f712682ebb1194b549
Keys4Ever <cubillagonzalo@outlook.com>
2025-05-07 08:33:44 -0300

    ⚡Feat: Add token validation on login and register controllers to redirect users to home if token is valid

--- Finished/controllers/login.controller.js
+++ Finished/controllers/login.controller.js
@@ -1,7 +1,18 @@
 angular.module('weatherApp')
 .controller('loginController', function($scope, AuthService, $location) {
+    
+    if(AuthService.getToken()) {
+        AuthService.isTokenValid().then(() => {
+            $location.path('/home');
+        }).catch(() => {
+            AuthService.removeToken();
+        });
+    }
+    
     $scope.credentials = { username: '', password: '' };
     $scope.error = null;
+
+
 
     $scope.login = function() {
         AuthService.login($scope.credentials).then(() => {
--- Finished/controllers/register.controller.js
+++ Finished/controllers/register.controller.js
@@ -1,5 +1,14 @@
 angular.module('weatherApp')
 .controller('registerController', function($scope, AuthService, $location) { 
+    if(AuthService.getToken()) {
+        AuthService.isTokenValid().then(() => {
+            $location.path('/home');
+        }).catch(() => {
+            AuthService.removeToken();
+        });
+    }
+  
+  
     $scope.credentials = { username: '', password: '' };
     $scope.error = null;
     $scope.register = function() {
